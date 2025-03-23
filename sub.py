62a2fdca294b5d6559ab377d68919ea56d42780a
Gaddam Deepika <gddamdeepika.321@gmail.com>
2025-03-23 11:50:28 +0530

    commit inialited for add,sub

---
+++ sub.py
@@ -0,0 +1,8 @@
+def sub_numbers(a, b):
+    return a - b  # Function to add two numbers
+
+# Calling the function and storing the result
+result = sub_numbers(5, 3)
+
+# Printing the result
+print("Diff:", result)
