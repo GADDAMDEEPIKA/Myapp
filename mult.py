11186faa1c21533cff17f74511c847d8988c0bb3
Gaddam Deepika <gddamdeepika.321@gmail.com>
2025-03-23 11:55:35 +0530

    commit for product

---
+++ mult.py
@@ -0,0 +1,8 @@
+def prdt_numbers(a, b):
+    return a * b  # Function to add two numbers
+
+# Calling the function and storing the result
+result = prdt_numbers(5, 3)
+
+# Printing the result
+print("Product:", result)
